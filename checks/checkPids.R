acb854bea66498236e5b35402a292bb9b2763478
Mateusz Żółtak <zozlak@zozlak.org>
2023-12-29 09:03:53 +0100

    checks/* script updates

--- checks/checkPids.R
+++ checks/checkPids.R
@@ -1,7 +1,48 @@
+# with
+# foo as (
+#   select p.id, p.value as pid, coalesce(r9.target_id, r8.target_id, r7.target_id, r6.target_id, r5.target_id, r4.target_id, r3.target_id, r2.target_id, r1.target_id, p.id) as topcolid
+#   from
+#   metadata p
+#   left join relations r1 on p.id         = r1.id and r1.property = 'https://vocabs.acdh.oeaw.ac.at/schema#isPartOf'
+#   left join relations r2 on r1.target_id = r2.id and r2.property = 'https://vocabs.acdh.oeaw.ac.at/schema#isPartOf'
+#   left join relations r3 on r2.target_id = r3.id and r3.property = 'https://vocabs.acdh.oeaw.ac.at/schema#isPartOf'
+#   left join relations r4 on r3.target_id = r4.id and r4.property = 'https://vocabs.acdh.oeaw.ac.at/schema#isPartOf'
+#   left join relations r5 on r4.target_id = r5.id and r5.property = 'https://vocabs.acdh.oeaw.ac.at/schema#isPartOf'
+#   left join relations r6 on r5.target_id = r6.id and r6.property = 'https://vocabs.acdh.oeaw.ac.at/schema#isPartOf'
+#   left join relations r7 on r6.target_id = r7.id and r7.property = 'https://vocabs.acdh.oeaw.ac.at/schema#isPartOf'
+#   left join relations r8 on r7.target_id = r8.id and r8.property = 'https://vocabs.acdh.oeaw.ac.at/schema#isPartOf'
+#   left join relations r9 on r8.target_id = r9.id and r9.property = 'https://vocabs.acdh.oeaw.ac.at/schema#isPartOf'
+#   where p.property = 'https://vocabs.acdh.oeaw.ac.at/schema#hasPid'
+# )
+# select foo.*, m1.value as topcolclass, min(m2.value) as topcollabel
+# from
+# foo
+# left join metadata m1 on foo.topcolid = m1.id and m1.property = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'
+# join metadata m2 on foo.topcolid = m2.id and m2.property = 'https://vocabs.acdh.oeaw.ac.at/schema#hasTitle'
+# group by 1, 2, 3, 4;
+
 library(dplyr)
-arche = readr::read_csv2('pids_arche.csv')
-hdl = readr::read_csv2('pids.csv')
-all = full_join(hdl %>% mutate(hdl = T), arche %>% rename(pid = arche_pid) %>% mutate(arche = T, pid = sub('https://hdl.handle.net/', '', pid))) %>% 
+arche = readr::read_csv2('pids_arche.csv') %>%
+  mutate(pid = sub('https://hdl.handle.net/', '', pid))
+hdl = readr::read_csv2('pids.csv') %>%
+  mutate(
+    data = if_else(grepl('^http', url), NA_character_, url),
+    url = if_else(grepl('^http', url), url, NA_character_)
+  ) %>%
+  filter(grepl('./.', pid))
+
+# ARCHE-only
+arche %>% 
+  anti_join(hdl) %>%
+  select(-topcolclass) %>%
+  tidyr::nest(id, pid)
+arche %>% 
+  anti_join(hdl) %>%
+  select(-topcolclass) %>%
+  arrange(topcollabel) %>%
+  readr::write_csv2('arche_only.csv', na = '')
+
+all = full_join(hdl %>% mutate(hdl = T), arche %>% mutate(arche = T, pid = sub('https://hdl.handle.net/', '', pid))) %>% 
   mutate(
     domain = sub('^(https?://[^/]+).*', '\\1', url),
     path = sub('^(https?://[^/]+)/([^/]+).*', '\\2', url)
@@ -9,23 +50,31 @@ all = full_join(hdl %>% mutate(hdl = T), arche %>% rename(pid = arche_pid) %>% m
   mutate(
     path = if_else(domain %in% c('http://www.bruckner-online.at', 'https://digitarium-app.acdh.oeaw.ac.at', 'https://ferdinand-korrespondenz.acdh.oeaw.ac.at'), '', path),
     status = case_when(
-      status == -3 ~ 'PID with empty redirect URL',
-      status == -2 ~ 'PID without redirect URL',
-      status == -1 ~ 'Network error',
-      status == 401 ~ 'Authorization required',
-      status == 403 ~ 'Access denied',
-      status == 404 ~ 'Redirect URL does not exist',
-      status == 200 ~ 'OK'
+      status == '400' ~ 'Bad request',
+      status == '401' ~ 'Authorization required',
+      status == '403' ~ 'Access denied',
+      status == '404' ~ 'Redirect URL does not exist',
+      status == '200' ~ 'OK',
+      TRUE ~ status
     )
   )
 all %>% group_by(arche, hdl) %>% summarize(n = n())
 all %>% group_by(status) %>% summarize(n = n())
 all %>% 
-  filter(status != 200) %>%
+  filter(status != 'OK') %>%
   group_by(arche, domain, path, status) %>%
   summarize(n = n()) %>%
   arrange(arche,  domain, path, status) %>%
   print(n = 1000)
-all %>% 
-  filter(is.na(hdl)) %>%
+
+# HTTP error
+d = all %>%
+  filter(status == 'HTTP error') %>%
+  select(id, pid, data) %>%
+  mutate(
+    url = purrr::map_chr(data, function(x) (jsonlite::fromJSON(jsonlite::fromJSON(x)$piddata) %>% filter(type == 'URL'))$parsed_data %>% unlist()),
+    error = sub('^.*cURL error [0-9]+: ([^:]+).*$', '\\1', data)
+  )
+d %>%
+  group_by(id, url, error) %>%
   summarize(n = n())
--- checks/checkPids.php
+++ checks/checkPids.php
@@ -1,9 +1,13 @@
 <?php
 use GuzzleHttp\Psr7\Request;
-include 'vendor/autoload.php';
+include '../vendor/autoload.php';
 $prefix = '21.11115';
 $limit = $argv[2] ?? 0;
-$c = new GuzzleHttp\Client(['auth' => ["user$prefix-01", $argv[1]], 'http_errors' => false]);
+$c = new GuzzleHttp\Client([
+    'auth' => ["user$prefix-01", $argv[1]], 
+    'http_errors' => false,
+    'timeout' => 5,
+]);
 $service = 'https://pid.gwdg.de/handles';
 $r = new Request('get', "$service/$prefix?limit=$limit");
 $resp = $c->send($r);
@@ -11,12 +15,15 @@ $pids = explode("\n", trim((string) $resp->getBody()));
 $pids = array_filter($pids, fn($x) => !str_contains($x, 'USER'));
 echo count($pids)." PIDs fetched\n";
 
-$pids = [];
 if (file_exists('pids.csv')) {
     $checked = explode("\n", trim(file_get_contents('pids.csv')));
     array_shift($checked); // header
     $checked = array_map(fn($x) => explode('/', explode(';', $x)[0])[1], $checked);
     $checked = array_combine($checked, $checked);
+    echo "status of " . count($checked) . " PIDs read\n";
+    $pids = array_filter($pids, fn($x) => !isset($checked[$x]));
+    unset($checked);
+    echo count($pids) . " PIDs left to check\n";
     $o = fopen('pids.csv', 'a');
 } else {
     $o = fopen('pids.csv', 'w');
@@ -24,14 +31,11 @@ if (file_exists('pids.csv')) {
 }
 
 $N = count($pids);
+$P = max(min($N / 100, 1000), 10);
 $n = 1;
 $t0 = time();
 foreach ($pids as $pid) {
-    if (isset($checked[$pid])) {
-        $N--;
-        continue;
-    }
-    if ($n % 100 === 0) {
+    if ($n % $P === 0) {
         $t = time() - $t0;
         $tl = (int) (($N - $n) / ($t / $n) / 60);
         $t = (int) ($t / 60);
@@ -42,16 +46,16 @@ foreach ($pids as $pid) {
         $d = json_decode((string) $r->getBody());
         $d = array_filter($d, fn($x) => $x->type === 'URL');
         if (!isset($d[0])) {
-            fwrite($o, "$prefix/$pid;-2;\n");
+            fwrite($o, "$prefix/$pid;no URL;" . json_encode((string) $r->getBody(),  JSON_UNESCAPED_SLASHES) . "\n");
         } elseif(empty(trim($d[0]->parsed_data))) {
-            fwrite($o, "$prefix/$pid;-3;\n");
+            fwrite($o, "$prefix/$pid;empty URL;" . json_encode((string) $r->getBody(),  JSON_UNESCAPED_SLASHES) . "\n");
         } else {
             $d = $d[0]->parsed_data;
             $r = $c->send(new Request('head', $d));
             fwrite($o, "$prefix/$pid;" . $r->getStatusCode() . ";$d\n");
         }
     } catch (Throwable $e) {
-        fwrite($o, "$prefix/$pid;-1;\n"); 
+        fwrite($o, "$prefix/$pid;HTTP error;" . json_encode(['message' => $e->getMessage(), 'piddata' => (string) $r->getBody()], JSON_UNESCAPED_SLASHES) . "\n"); 
     }
     $n++;
 }
